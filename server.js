51f7e9a6a01429b5db38d67ee22fa5bd348c5597
Andrewphanpuspo <andrewphanpuspo2000@gmail.com>
2023-08-21 16:12:01 +1000

    update

--- server.js
+++ server.js
@@ -12,19 +12,49 @@ import borrowrouter from "./router/borrowRouter.js";
 import { auth } from "./AuthMiddleware/authMiddleware.js";
 import commentRouter from "./router/commentRouter.js";
 
+import path from "path";
 const app = express();
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 8000;
+console.log(__dirname);
 //middleware
+app.use(express.static(__dirname + "/build"));
+console.log("dirname" + __dirname);
 app.use(morgan());
 app.use(express.json());
 app.use(cors());
-mongoConnect();
+// mongoConnect();
 //api
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/book", bookrouter);
 app.use("/api/v1/borrow", auth, borrowrouter);
 app.use("/api/v1/comment", commentRouter);
+//send file
+app.use("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//database connection in cyclic
+// const db =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.MONGO_CLIENT
+//     : "mongodb://localhost:27017/nottododb";
+
+mongoose
+  .connect(process.env.MONGO_CLIENT)
+  .then(() => {
+    console.log("Connected to mongo");
+    app.listen(PORT, (error) => {
+      console.log("Connected to port");
+      error
+        ? console.log(error.message)
+        : console.log(`server run in port http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
 //root router
 app.use("/", (req, res) => {
   res.json({
